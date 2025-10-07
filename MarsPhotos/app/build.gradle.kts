9a0031230eba1670a532b0bb848838ca8abf65c2
pstet <123316024+pcs03@users.noreply.github.com>
2025-10-07 17:57:10 +0200

    MarsPhotos: Add coil library dependency

--- MarsPhotos/app/build.gradle.kts
+++ MarsPhotos/app/build.gradle.kts
@@ -79,6 +79,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("io.coil-kt:coil-compose:2.7.0")
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
