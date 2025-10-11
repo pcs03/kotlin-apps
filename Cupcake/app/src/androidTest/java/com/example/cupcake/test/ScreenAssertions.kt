c543f28a636bdc12344053fb76c6880370733e3e
pstet <123316024+pcs03@users.noreply.github.com>
2025-10-11 17:39:39 +0200

    CLEANUP: Remove redundant repos

--- Cupcake/app/src/androidTest/java/com/example/cupcake/test/ScreenAssertions.kt
+++
@@ -1,8 +0,0 @@
-package com.example.cupcake.test
-
-import androidx.navigation.NavController
-import junit.framework.Assert.assertEquals
-
-fun NavController.assertCurrentRouteName(expectedRouteName: String) {
-    assertEquals(expectedRouteName, currentBackStackEntry?.destination?.route)
-}
--- Unscramble/app/src/main/java/com/example/unscramble/ui/GameUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.unscramble.ui
-
-data class GameUiState(
-    val currentScrambledWord: String = "",
-    val isGuessedWrong: Boolean = false,
-    val currentWordCount: Int = 1,
-    val score: Int = 0,
-    val isGameOver: Boolean = false,
-)
