fe1a7e7bebef5593a475245a557d88633886d9ac
pstet <123316024+pcs03@users.noreply.github.com>
2025-11-20 15:04:01 +0100

    Sample: Delete filler

--- Sample/app/src/main/java/nl/pcstet/sample/MainActivity.kt
+++ Sample/app/src/main/java/nl/pcstet/sample/MainActivity.kt
@@ -19,29 +19,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SampleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Text("Hello, world")
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SampleTheme {
-        Greeting("Android")
     }
 }
