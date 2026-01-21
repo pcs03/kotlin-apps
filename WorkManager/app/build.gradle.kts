1630aba95e6cb80ecff7c777585c598da5fd9e0c
pstet <123316024+pcs03@users.noreply.github.com>
2026-01-21 13:52:15 +0100

    WorkManager: Finish app

--- WorkManager/app/build.gradle.kts
+++ WorkManager/app/build.gradle.kts
@@ -84,4 +84,8 @@ dependencies {
     implementation("androidx.work:work-runtime-ktx:2.11.0")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.work:work-testing:2.11.0")
 }
